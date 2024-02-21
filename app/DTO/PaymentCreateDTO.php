67d628f92f135f7859ac79277dd54e651d814293
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-21 19:43:40 -0300

    add payment create dto

--- app/DTO/PaymentCreateDTO.php
+++ app/DTO/PaymentCreateDTO.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PaymentCreateDTO
 {
+    public readonly array $requestData;
     public readonly string $transactionAmount;
     public readonly int $installments;
     public readonly string $token;
@@ -13,50 +14,89 @@ class PaymentCreateDTO
     public readonly string $payerEmail;
     public readonly string $payerIdentificationType;
     public readonly string $payerIdentificationNumber;
+    public readonly string $payerEntityType;
+    public readonly string $payerType;
 
-    private const REQUEST_BASE = [
-        'transaction_amount' => null,
-        'installments' => null,
-        'token' => null,
-        'payment_method_id' => null,
-        'payer' => [
-            'email' => null,
-            'identification' => [
-                'type' => null,
-                'number' => null
-            ],
-        ],
-    ];
+    private const TRANSACTION_AMOUNT_FIELD = 'transaction_amount';
+    private const INSTALLMENTS_FIELD = 'installments';
+    private const TOKEN_FIELD = 'token';
+    private const PAYMENT_METHOD_ID_FIELD = 'payment_method_id';
+    private const PAYER_FIELD = 'payer';
+    private const PAYER_EMAIL_FIELD = 'email';
+    private const PAYER_IDENTIFICATION_FIELD = 'identification';
+    private const PAYER_IDENTIFICATION_TYPE_FIELD = 'type';
+    private const PAYER_IDENTIFICATION_NUMBER_FIELD = 'number';
+    private const PAYER_ENTITY_TYPE = 'individual';
+    private const PAYER_TYPE = 'customer';
 
     public function __construct(array $requestData)
     {
-        $this->verifyBaseFieldsExists($requestData);
-        $this->validateData($requestData);
+        $this->requestData = $requestData;
+        $this->validateData();
         $this->build();
     }
 
-    private function verifyBaseFieldsExists(array $requestData): void
+    private function validateData(): void
     {
-//        dd(array_keys($requestData));
-    }
+        if (!is_float($this->requestData[self::TRANSACTION_AMOUNT_FIELD])) {
+            $this->throwInvalidValueException();
+        }
 
-    private function validateData(array $requestData): void
-    {
-        if (empty($requestData)) {
-            $message = 'Payment not provided in the request body';
+        if (!is_int($this->requestData[self::INSTALLMENTS_FIELD])) {
+            $this->throwInvalidValueException();
+        }
 
-            throw new HttpResponseException(
-                response()->json([
-                    'message' => $message,
-                ], Response::HTTP_BAD_REQUEST)
-            );
+        if (!is_string($this->requestData[self::TOKEN_FIELD])) {
+            $this->throwInvalidValueException();
+        }
+
+        if (!is_string($this->requestData[self::PAYMENT_METHOD_ID_FIELD])){
+            $this->throwInvalidValueException();
+        }
+
+        $payerData = $this->requestData[self::PAYER_FIELD];
+        if (!is_string($payerData[self::PAYER_EMAIL_FIELD])) {
+            $this->throwInvalidValueException();
+        }
+
+        $payerIdentificationData = $payerData[self::PAYER_IDENTIFICATION_FIELD];
+        if (!is_string($payerIdentificationData[self::PAYER_IDENTIFICATION_TYPE_FIELD])) {
+            $this->throwInvalidValueException();
         }
 
 
+        if (!is_string($payerIdentificationData[self::PAYER_IDENTIFICATION_NUMBER_FIELD])) {
+            $this->throwInvalidValueException();
+        }
 
     }
 
-    private function build()
+    private function throwInvalidValueException(): void
     {
+        $message = 'Invalid payment provided.The possible reasons are:' .
+            'A field of the provided payment was null or with invalid values';
+
+        throw new HttpResponseException(
+            response()->json([
+                'message' => $message,
+            ], Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+
+    private function build(): void
+    {
+        $payerData = $this->requestData[self::PAYER_FIELD];
+        $payerIdentificationData = $payerData[self::PAYER_IDENTIFICATION_FIELD];
+
+        $this->transactionAmount = $this->requestData[self::TRANSACTION_AMOUNT_FIELD];
+        $this->installments = $this->requestData[self::INSTALLMENTS_FIELD];
+        $this->token = $this->requestData[self::TOKEN_FIELD];
+        $this->paymentMethodId = $this->requestData[self::PAYMENT_METHOD_ID_FIELD];
+        $this->payerEmail = $payerData[self::PAYER_EMAIL_FIELD];
+
+        $this->payerIdentificationType = $payerIdentificationData[self::PAYER_IDENTIFICATION_TYPE_FIELD];
+        $this->payerIdentificationNumber = $payerIdentificationData[self::PAYER_IDENTIFICATION_NUMBER_FIELD];
+        $this->payerEntityType = self::PAYER_ENTITY_TYPE;
+        $this->payerType = self::PAYER_TYPE;
     }
 }
