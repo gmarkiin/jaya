2fa0811c175502ba63840641e2903debc5cfbcb0
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-22 19:19:55 -0300

    add payment create request test

---
+++ tests/Feature/Requests/PaymentCreateRequestTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Requests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PaymentCreateRequestTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testPaymentCreateSuccessful()
+    {
+        $response = $this
+            ->json('POST', '/api/rest/payments', [
+                'transaction_amount' => 321.32,
+                'installments' => 2,
+                'token' => 'ae4e50b2a8f3h6d9f2c3a4b5d6e7f8g9',
+                'payment_method_id' => 'master',
+                'payer' => [
+                    'email' => "example_random@gmail.com",
+                    'identification' => [
+                        'type' =>  "CPF",
+                        'number' =>  "12345678909",
+                    ]
+                ]
+            ]);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('payments', [
+            'token' => 'ae4e50b2a8f3h6d9f2c3a4b5d6e7f8g9'
+        ]);
+    }
+}
