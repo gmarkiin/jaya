9dba33add213ebf12a67642f45c42f72fbeeee89
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-22 19:45:55 -0300

    add failed scenario

--- tests/Feature/Requests/PaymentCreateRequestTest.php
+++ tests/Feature/Requests/PaymentCreateRequestTest.php
@@ -32,4 +32,29 @@ class PaymentCreateRequestTest extends TestCase
             'token' => 'ae4e50b2a8f3h6d9f2c3a4b5d6e7f8g9'
         ]);
     }
+
+    public function testPaymentCreateFailed()
+    {
+        $response = $this
+            ->json('POST', '/api/rest/payments', [
+                'transaction_amount' => 321.32,
+                'token' => 'ae4e50b2a8f3h6d9f2c3a4b5d6e7f8g9',
+                'payment_method_id' => 'master',
+                'payer' => [
+                    'email' => "example_random@gmail.com",
+                    'identification' => [
+                        'type' =>  "CPF",
+                        'number' =>  "12345678909",
+                    ]
+                ]
+            ]);
+
+        $response->assertBadRequest();
+        $response->assertJson(
+            [
+                'message' => 'Payment not provided in the request body'
+            ]
+        );
+
+    }
 }
