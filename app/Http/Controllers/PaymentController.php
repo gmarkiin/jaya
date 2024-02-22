0f9c2c8e0be390d4ed3c2ad7daa486078f2f020c
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-22 19:20:13 -0300

    refactor return

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,21 +5,25 @@ namespace App\Http\Controllers;
 use App\Db\Payment\PaymentDb;
 use App\Domain\Payment\Payment;
 use App\DTO\PaymentCreateDTO;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class PaymentController extends BaseController
 {
-    public function create(Request $request): array
+    public function create(Request $request): JsonResponse
     {
         $paymentCreateDto = new PaymentCreateDTO($request->toArray());
         $payment = new Payment(new PaymentDb());
         $payment->createDTO = $paymentCreateDto;
         $payment->create();
 
-        return [
-            'id' => $payment->createDTO->id->value,
-            'created_at' => $payment->createDTO->createdAt->value
-        ];
+        return response()
+            ->json(
+                [
+                    'id' => $payment->createDTO->id->value,
+                    'created_at' => $payment->createDTO->createdAt->value
+                ]
+            )->setStatusCode(201);
     }
 }
