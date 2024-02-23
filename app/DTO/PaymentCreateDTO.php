b1e32ec074df7df5786c1654ffc3be7018eae06c
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-23 13:40:22 -0300

    refactor payment create dto

--- app/DTO/PaymentCreateDTO.php
+++ app/DTO/PaymentCreateDTO.php
@@ -12,7 +12,6 @@ use Ramsey\Uuid\Uuid;
 
 class PaymentCreateDTO
 {
-    private readonly array $requestData;
     public readonly FloatVO $transactionAmount;
     public readonly IntegerVO $installments;
     public readonly StringVO $token;
@@ -26,37 +25,22 @@ class PaymentCreateDTO
     public readonly PaymentStatusVO $status;
     public readonly StringVO $createdAt;
 
-    private const TRANSACTION_AMOUNT_FIELD = 'transaction_amount';
-    private const INSTALLMENTS_FIELD = 'installments';
-    private const TOKEN_FIELD = 'token';
-    private const PAYMENT_METHOD_ID_FIELD = 'payment_method_id';
-    private const PAYER_FIELD = 'payer';
-    private const PAYER_EMAIL_FIELD = 'email';
-    private const PAYER_IDENTIFICATION_FIELD = 'identification';
-    private const PAYER_IDENTIFICATION_TYPE_FIELD = 'type';
-    private const PAYER_IDENTIFICATION_NUMBER_FIELD = 'number';
     private const PAYER_ENTITY_TYPE = 'individual';
     private const PAYER_TYPE = 'customer';
 
     public function __construct(array $requestData)
     {
-        $this->requestData = $requestData;
-        $this->build();
-    }
+        $payerData = $requestData['payer'];
+        $payerIdentificationData = $payerData['identification'];
 
-    private function build(): void
-    {
-        $payerData = $this->requestData[self::PAYER_FIELD];
-        $payerIdentificationData = $payerData[self::PAYER_IDENTIFICATION_FIELD];
+        $this->transactionAmount = new FloatVO($requestData['transaction_amount']);
+        $this->installments = new IntegerVO($requestData['installments']);
+        $this->token = new StringVO($requestData['token']);
+        $this->paymentMethodId = new StringVO($requestData['payment_method_id']);
+        $this->payerEmail = new StringVO($payerData['email']);
 
-        $this->transactionAmount = new FloatVO($this->requestData[self::TRANSACTION_AMOUNT_FIELD]);
-        $this->installments = new IntegerVO($this->requestData[self::INSTALLMENTS_FIELD]);
-        $this->token = new StringVO($this->requestData[self::TOKEN_FIELD]);
-        $this->paymentMethodId = new StringVO($this->requestData[self::PAYMENT_METHOD_ID_FIELD]);
-        $this->payerEmail = new StringVO($payerData[self::PAYER_EMAIL_FIELD]);
-
-        $this->payerIdentificationType = new StringVO($payerIdentificationData[self::PAYER_IDENTIFICATION_TYPE_FIELD]);
-        $this->payerIdentificationNumber = new StringVO($payerIdentificationData[self::PAYER_IDENTIFICATION_NUMBER_FIELD]);
+        $this->payerIdentificationType = new StringVO($payerIdentificationData['type']);
+        $this->payerIdentificationNumber = new StringVO($payerIdentificationData['number']);
         $this->payerEntityType = new StringVO(self::PAYER_ENTITY_TYPE);
         $this->payerType = new StringVO(self::PAYER_TYPE);
 
