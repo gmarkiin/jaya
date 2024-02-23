26088e6099f6a8e3ecbaa1d8e8f6172a732e5a65
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-23 13:28:26 -0300

    add payments route

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('/rest')->controller(PaymentController::class)->group(function () {
     Route::post('/payments', 'create');
+    Route::get('/payments', 'listAllPayments');
 });
