987f4ebae552876de55fcee46a19eaa553559d6f
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-23 13:28:42 -0300

    add remove xdebug

--- app/Domain/Payment/Payment.php
+++ app/Domain/Payment/Payment.php
@@ -4,11 +4,13 @@ namespace App\Domain\Payment;
 
 use App\Db\Payment\PaymentPersistenceInterface;
 use App\DTO\PaymentCreateDTO;
+use App\DTO\PaymentListDTO;
 
 class Payment
 {
     private ?PaymentPersistenceInterface $paymentPersistence;
     public ?PaymentCreateDTO $createDTO = null;
+    public array $paymentsList;
     public string $notificationUrl = 'i dont know YET';
 
     public function __construct(PaymentPersistenceInterface $paymentPersistence = null)
@@ -19,6 +21,18 @@ class Payment
     public function create(): void
     {
         $this->paymentPersistence->create($this);
+    }
+
+    public function listAllPayments(): array
+    {
+        $this->paymentPersistence->listAllPayments($this);
+
+        $payments = [];
+        foreach ($this->paymentsList as $payment) {
+            $payments[] = $payment->getAllProperties();
+        }
+
+        return $payments;
     }
 
 }
