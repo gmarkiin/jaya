2406fad9aae45839ccc983a78d1efe98a433ad91
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-24 19:48:34 -0300

    refactor object values

--- app/Domain/Payment/ValueObject/PaymentStatusVO.php
+++ app/Domain/Payment/ValueObject/PaymentStatusVO.php
@@ -3,24 +3,33 @@
 namespace App\Domain\Payment\ValueObject;
 
 use App\Enum\PaymentStatusEnum;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Symfony\Component\HttpFoundation\Response;
+use App\Exceptions\InvalidPropertyValueException;
 
 class PaymentStatusVO
 {
+    /**
+     * @throws InvalidPropertyValueException
+     */
     public function __construct(public readonly mixed $value)
     {
         $this->validateStatus();
     }
 
+    /**
+     * @throws InvalidPropertyValueException
+     */
     private function validateStatus(): void
     {
+        if (empty($this->value)) {
+            throw new InvalidPropertyValueException('The status cannot be empty');
+        }
+
+        if (!is_string($this->value)) {
+            throw new InvalidPropertyValueException("The status '$this->value' need's be a string");
+        }
+
         if (!in_array($this->value, PaymentStatusEnum::availableStatus())) {
-            throw new HttpResponseException(
-                response()->json([
-                    'message' => 'Invalid payment status',
-                ], Response::HTTP_INTERNAL_SERVER_ERROR)
-            );
+            throw new InvalidPropertyValueException("The status '$this->value' isn't a available status");
         }
     }
 }
