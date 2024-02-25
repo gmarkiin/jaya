6fefc4ab706ebc76ada63f0cb12db4886058bd45
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-25 16:47:49 -0300

    add swagger generate doc command

---
+++ app/Console/Commands/Swagger.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class Swagger extends Command
+{
+
+
+    protected $signature = 'swagger';
+
+    protected $description = 'This command generate swagger api documentation';
+
+    public function handle()
+    {
+        $openApi = \OpenApi\Generator::scan([config('swagger.sources')]);
+        file_put_contents("public/api-documentation/swagger.json", $openApi->toJson());
+
+        $this->info('Api documentation generated successfully');
+        return CommandAlias::SUCCESS;
+    }
+}
