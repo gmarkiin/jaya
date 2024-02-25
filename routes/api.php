6e98063c3a443bdf920a961a803433188fd32307
Marcos Gabriel M <gmarcos38@gmail.com>
2024-02-25 16:45:29 -0300

    add swagger endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PaymentController;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,8 @@ Route::prefix('/rest')->controller(PaymentController::class)->group(function () 
     Route::get('/payments/{id}', 'listPayment');
     Route::patch('/payments/{id}', 'confirmPayment');
     Route::delete('/payments/{id}', 'cancelPayment');
+});
+
+Route::get('/api-docs', function () {
+    return Response::file(public_path('/api-documentation/swagger.json'));
 });
